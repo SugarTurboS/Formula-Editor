da6fb32753af995de8b3adf34abcdda291d0682a
王登芝 <wangdengzhi@cvte.com>
2020-04-14 11:40:36 +0800

    [feature][what]新增编辑器用例

--- test/project/add.js
+++
@@ -1,9 +0,0 @@
-/*
- * @Author: Demian
- * @Date: 2020-04-13 20:15:17
- * @LastEditor: Demian
- * @LastEditTime: 2020-04-13 20:15:34
- */
-function add(x, y) {
-  return x + y;
-}
--- test/unit/test.js
+++ test/unit/test.js
@@ -1,13 +1,9 @@
 var expect = chai.expect;
 
-describe('测试add函数', function () {
-  it('1加1等于2', function () {
-    var sum = add(1, 1);
-    expect(sum).to.equal(2);
-  });
+describe('测试工厂', function () {
 
   it('1加2等于3', function () {
-    var sum = add(1, 2);
-    expect(sum).to.equal(3);
+    console.log(factory);
+    expect(factory).to.have.property('ready');
   });
 });
